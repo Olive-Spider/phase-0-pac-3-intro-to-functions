92a0d3c3926f3372a6a3430c11e5d091a5ec97f7
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-01-06 00:27:01 +0300

    This is just for practice

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Follow along with the examples here
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+
+  sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  
+  sayHelloTo("Guadalupe"); // "Hello, Guadalupe!"
+  sayHelloTo("Jane"); // "Hello, Jane!"
+  sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+  sayHelloTo(1); 
